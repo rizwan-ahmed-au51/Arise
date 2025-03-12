96fe31d48645f7185d02b1d93783222bc7c139d3
Rizwan Ahmed <rizwanahmed6229@gmail.com>
2025-03-12 16:22:30 +0530

    Added header/footer compponent in app.js

--- client/arise/src/App.js
+++ client/arise/src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import Routing from './Routing';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
 
     <div className='App'>
+      <Header />
       <Routing />
+      <Footer />
     </div>
 
   );
